557f8349676a8761267d412532f1a38935403798
huongtt182570 <huonguni2306@gmail.com>
2023-11-19 01:38:47 +0700

    init db

---
+++ prisma/migrations/20231118183530_init/migration.sql
@@ -0,0 +1,114 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'DOCTER', 'PATIENT');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "AppointmentStatus" AS ENUM ('PENDING', 'CONFIRMED', 'IN_PROGRESS', 'COMPLETED', 'CANCELED_BY_DOCTER', 'CANCELED_BY_PATIENT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "fullname" TEXT,
+    "address" TEXT,
+    "birthday" TIMESTAMP(3),
+    "gender" "Gender" NOT NULL DEFAULT 'OTHER',
+    "role" "Role" NOT NULL DEFAULT 'PATIENT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Docter" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "speciality" TEXT NOT NULL,
+    "degree" TEXT NOT NULL,
+    "experience" TEXT NOT NULL,
+
+    CONSTRAINT "Docter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Rating" (
+    "id" SERIAL NOT NULL,
+    "patientId" INTEGER NOT NULL,
+    "docterId" INTEGER NOT NULL,
+    "rate" DOUBLE PRECISION NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Rating_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Appointment" (
+    "id" SERIAL NOT NULL,
+    "patientId" INTEGER NOT NULL,
+    "docterId" INTEGER NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "status" "AppointmentStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MedicalHistory" (
+    "id" SERIAL NOT NULL,
+    "patientId" INTEGER NOT NULL,
+    "docterId" INTEGER NOT NULL,
+    "symptons" TEXT NOT NULL,
+    "diagnosis" TEXT NOT NULL,
+    "treatment" TEXT NOT NULL,
+    "prescription" TEXT NOT NULL,
+    "admissionDate" TIMESTAMP(3) NOT NULL,
+    "dischargeDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MedicalHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Docter_userId_key" ON "Docter"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Docter" ADD CONSTRAINT "Docter_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_docterId_fkey" FOREIGN KEY ("docterId") REFERENCES "Docter"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_docterId_fkey" FOREIGN KEY ("docterId") REFERENCES "Docter"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MedicalHistory" ADD CONSTRAINT "MedicalHistory_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MedicalHistory" ADD CONSTRAINT "MedicalHistory_docterId_fkey" FOREIGN KEY ("docterId") REFERENCES "Docter"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
