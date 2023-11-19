5f9983b343e5b7285f0ba1dbb4e0620d416f534d
huongtt182570 <huonguni2306@gmail.com>
2023-11-19 23:56:41 +0700

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV_KEY } from './constants';
+import { setupSwagger } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix(prefix);
   app.enableCors({ origin: '*' });
+
+  setupSwagger(app, configService);
 
   await app.listen(port || 3000);
 }
---
+++ src/utils/swagger.ts
@@ -0,0 +1,22 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ENV_KEY } from 'src/constants';
+
+export const setupSwagger = (
+  app: INestApplication,
+  configService: ConfigService
+) => {
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>(ENV_KEY.APP_NAME))
+    .setDescription('Clinic management server api documentation')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      basePath: '/v1',
+    },
+  });
+};
