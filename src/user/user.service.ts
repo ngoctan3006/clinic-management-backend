27a4160489a36945c7b105528fbd5ab450ed4560
huongtt182570 <huonguni2306@gmail.com>
2023-11-21 13:44:20 +0700

    Add role field to CreateUserDto and implement create method in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Gender, Role, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { hashPassword } from 'src/utils';
+import { CreateUserDto } from './dtos';
 
 @Injectable()
 export class UserService {
@@ -20,5 +22,35 @@ export class UserService {
 
   async findByPhone(phone: string): Promise<User> {
     return await this.prisma.user.findUnique({ where: { phone } });
+  }
+
+  async create(data: CreateUserDto): Promise<User> {
+    const { username, password, phone, email, gender, role, ...rest } = data;
+    const usernameExist = await this.findByUsername(username);
+    if (usernameExist) {
+      throw new BadRequestException('Username already exists');
+    }
+
+    const emailExist = await this.findByEmail(email);
+    if (emailExist) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const phoneExist = await this.findByPhone(phone);
+    if (phoneExist) {
+      throw new BadRequestException('Phone already exists');
+    }
+
+    return await this.prisma.user.create({
+      data: {
+        ...rest,
+        username,
+        phone,
+        email,
+        password: await hashPassword(password),
+        gender: Gender[gender] || Gender.OTHER,
+        role: Role[role] || Role.PATIENT,
+      },
+    });
   }
 }
