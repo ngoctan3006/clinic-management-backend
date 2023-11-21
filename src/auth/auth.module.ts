5cbac8cb53c08cce975e8ac51912fe91b9105cb5
huongtt182570 <huonguni2306@gmail.com>
2023-11-21 13:47:14 +0700

    Refactor auth module imports and add JWT configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ENV_KEY } from 'src/constants';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>(ENV_KEY.JWT_ACCESS_TOKEN_SECRET),
+        signOptions: {
+          expiresIn: configService.get<number>(
+            ENV_KEY.JWT_ACCESS_TOKEN_EXPIRATION_TIME
+          ),
+        },
+      }),
+    }),
+    UserModule,
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
