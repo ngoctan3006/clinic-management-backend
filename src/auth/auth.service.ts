59efca34f25ed0a80e5f94ef54d45399941b23e8
huongtt182570 <huonguni2306@gmail.com>
2023-11-21 13:50:14 +0700

    Add generateToken method to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { ENV_KEY } from 'src/constants';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
+  ) {}
+
+  async generateToken(user: User): Promise<{
+    accessToken: string;
+    refreshToken: string;
+  }> {
+    const payload = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+    };
+    const accessToken = await this.jwtService.signAsync(payload);
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>(ENV_KEY.JWT_REFRESH_TOKEN_SECRET),
+      expiresIn: this.configService.get<number>(
+        ENV_KEY.JWT_REFRESH_TOKEN_EXPIRATION_TIME
+      ),
+    });
+    return { accessToken, refreshToken };
+  }
+}
