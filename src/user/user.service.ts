5b6398b16baea11125a6ddf45b216b76e2cd975f
huongtt182570 <huonguni2306@gmail.com>
2023-11-21 13:27:22 +0700

    Refactor user module and add user service methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: number): Promise<User> {
+    return await this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    return await this.prisma.user.findUnique({ where: { username } });
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return await this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findByPhone(phone: string): Promise<User> {
+    return await this.prisma.user.findUnique({ where: { phone } });
+  }
+}
