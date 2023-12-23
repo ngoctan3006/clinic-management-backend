6956c1e7a069afd49ac403d566ed67b85822cd40
huongtt182570 <huonguni2306@gmail.com>
2023-12-23 21:21:52 +0700

    get all user

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/common/decorators';
+import { IQuery } from 'src/common/dtos';
 import { AdminService } from './admin.service';
 
 @ApiTags('admin')
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @ApiBearerAuth()
+  @Roles(Role.ADMIN)
+  @Get('users')
+  async getAllUser(@Query() query: IQuery) {
+    return this.adminService.getAllUser(query);
+  }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { UserWithoutPassword } from 'src/auth/dtos';
+import { IQuery, IResponse } from 'src/common/dtos';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
 
@@ -8,4 +10,36 @@ export class AdminService {
     private readonly prisma: PrismaService,
     private readonly userService: UserService,
   ) {}
+
+  async getAllUser(query: IQuery): Promise<IResponse<UserWithoutPassword[]>> {
+    const { page, pageSize } = query;
+    const skip = (page - 1) * pageSize;
+    const total = await this.prisma.user.count();
+    const data = await this.prisma.user.findMany({
+      skip,
+      take: pageSize,
+      select: {
+        id: true,
+        phone: true,
+        fullname: true,
+        email: true,
+        address: true,
+        birthday: true,
+        gender: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return {
+      success: true,
+      message: 'Get all user success',
+      data,
+      pagination: {
+        page,
+        pageSize,
+        total,
+      },
+    };
+  }
 }
