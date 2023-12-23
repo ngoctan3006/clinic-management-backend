8369ee4ad588b1e8e24caafaef6f9efcdbc69f96
huongtt182570 <huonguni2306@gmail.com>
2023-12-23 16:52:42 +0700

    Add transformToInt function

--- src/common/utils/transformer.util.ts
+++ src/common/utils/transformer.util.ts
@@ -2,3 +2,5 @@ export const transformValue = ({ value }) => value || undefined;
 
 export const transformToDate = ({ value }) =>
   value ? new Date(value) : undefined;
+
+export const transformToInt = ({ value }) => parseInt(value) || undefined;
