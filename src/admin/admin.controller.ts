621e83fcc8227c17b1af4635b04816d08e0ab5cc
huongtt182570 <huonguni2306@gmail.com>
2023-12-27 15:57:42 +0700

    admin create doctor api

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,19 +1,39 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
+import { UserWithoutPassword } from 'src/auth/dtos';
 import { Roles } from 'src/common/decorators';
-import { IQuery } from 'src/common/dtos';
+import { IQuery, IResponse } from 'src/common/dtos';
 import { AdminService } from './admin.service';
+import { CreateDoctorDto, IDoctor } from './dtos';
 
 @ApiTags('admin')
+@Roles(Role.ADMIN)
+@ApiBearerAuth()
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @ApiBearerAuth()
-  @Roles(Role.ADMIN)
   @Get('users')
-  async getAllUser(@Query() query: IQuery) {
+  async getAllUser(
+    @Query() query: IQuery,
+  ): Promise<IResponse<UserWithoutPassword[]>> {
     return this.adminService.getAllUser(query);
+  }
+
+  @ApiConsumes(
+    'application/x-www-form-urlencoded',
+    'multipart/form-data',
+    'application/json',
+  )
+  @Post('doctor')
+  async createDoctor(
+    @Body() data: CreateDoctorDto,
+  ): Promise<IResponse<IDoctor>> {
+    return {
+      success: true,
+      message: 'Create doctor successfully',
+      data: await this.adminService.createDoctor(data),
+    };
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaClient, Role } from '@prisma/client';
+import * as runtime from '@prisma/client/runtime/library';
 import { UserWithoutPassword } from 'src/auth/dtos';
 import { IQuery, IResponse } from 'src/common/dtos';
+import { hashPassword } from 'src/common/utils';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
+import { CreateDoctorDto, IDoctor } from './dtos';
 
 @Injectable()
 export class AdminService {
@@ -41,5 +45,62 @@ export class AdminService {
         total,
       },
     };
+  }
+
+  async createDoctor(data: CreateDoctorDto): Promise<IDoctor> {
+    const { phone, email } = data;
+    const phoneExist = await this.userService.findByPhone(phone);
+    if (phoneExist) {
+      throw new BadRequestException({
+        success: false,
+        message: 'Phone number already exists',
+        data: null,
+      });
+    }
+    if (email) {
+      const emailExist = await this.userService.findByEmail(email);
+      if (emailExist) {
+        throw new BadRequestException({
+          success: false,
+          message: 'Email already exists',
+          data: null,
+        });
+      }
+    }
+
+    try {
+      const newDoctor: IDoctor = await this.prisma.$transaction(
+        async (prisma: Omit<PrismaClient, runtime.ITXClientDenyList>) => {
+          const { speciality, degree, experience, ...userData } = data;
+          const user = await prisma.user.create({
+            data: {
+              ...userData,
+              role: Role.DOCTOR,
+              password: await hashPassword('123456'),
+            },
+          });
+          const doctor = await prisma.doctor.create({
+            data: {
+              userId: user.id,
+              speciality,
+              degree,
+              experience,
+            },
+          });
+          delete user.password;
+          return {
+            ...user,
+            doctor,
+          };
+        },
+      );
+      return newDoctor;
+    } catch (error) {
+      throw new BadRequestException({
+        success: false,
+        message: error?.message || 'Create doctor failed',
+        data: null,
+      });
+    }
   }
 }
